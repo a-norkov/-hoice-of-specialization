d81c25e2b60dfcf55cb4dff14c500115bb35d75a
Aleksei Norkov <user_317215@icloud.com>
2023-03-12 15:37:45 +0300

    added solution for task, readme.md updated

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите массив строк, через запятую. пример Hello, 123!rwef, 23r23e, World!, 33");
+string[] stringArray = Console.ReadLine().Split(',');
+string[] parsedStringArray = new string [0];
+
+// проходим циклом по каждому элементу массива
+for (int i = 0; i < stringArray.Length; i++)
+{
+    // удаляем пробелы, введенные пользователем (если они есть)
+    stringArray[i] = stringArray[i].Trim();
+
+    // проверяем условие, если длина строки меньше или равна 3, то добавляем ее в новый массив.
+    if (stringArray[i].Length <= 3)
+    {
+        // добавляем элемент в массив
+        parsedStringArray =  parsedStringArray.Append (stringArray[i]).ToArray();
+    }
+
+}
+
+Console.WriteLine("\nМассив состоящий из элементов с длиной строки меньшей либо равной 3:");
+// вывод в консоль нового массива
+for (int i = 0; i < parsedStringArray.Length; i++)
+{
+    Console.Write ($"'{parsedStringArray[i]}' ");
+}
